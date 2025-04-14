c95cdba9e0b2390f3c90169ed3c62d1eaa207f76
WindaAlfiraHB <12450122841@students.uin-suska.ac.id>
2025-04-15 01:10:17 +0700

    mobileGame.java

--- mobileGame.java
+++ mobileGame.java
@@ -11,6 +11,45 @@ public class mobileGame {
         this.rating = rating;
     }
 
+    public mobileGame(String name, String developer) {
+        this.name = name;
+        this.developer = developer;
+        this.genre = "Unknown";
+        this.rating = 0.0f;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDeveloper() {
+        return developer;
+    }
+
+    public void setDeveloper(String developer) {
+        this.developer = developer;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
     @Override
     public String toString() {
         return "mobileGame{" +
